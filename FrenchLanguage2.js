8102073e49de3af1f52700ee03a8d1ad7af01d86
dventura-tsr <98617967+dventura-tsr@users.noreply.github.com>
2023-07-10 09:19:14 -0700

    Add files via upload

---
+++ FrenchLanguage2.js
@@ -0,0 +1,111 @@
+/**
+ * Please see Chat Console documentation for full details on implement custom language file.
+ * - Edit this file with your custom language changes.
+ * - Host this file via HTTPS on your server.
+ * - Pass the URL to your hosted custom lang file as the `lang` options to Five9SocialWidget.addWidget()
+ */
+// eslint-disable-next-line no-undef
+chatLangCallback({
+  "APPLICATION_TYPE": "ChatConsole",
+  "S_PAGE_TITLE": "Chat",
+  "S_CLOSE": "Fermer",
+  "S_PROVIDER": "<span class='provider-text'>Provided by <span class='provider-five'>Five</span><span class='provider-nine'>9</span></span>",
+  "S_NO_NAME": "Me",
+  "S_INITIALIZATION_PAGE": {
+    "S_CONTENT": "Lancer le chat..."
+  },
+  "S_WAITING_PAGE": {
+    "S_CONTENT": "E-mail d'initiation..."
+  },
+  "S_INFORMATION_PAGE": {
+    "S_PROFILE_LABEL": "Profile",
+    "S_NAME_LABEL": "nom",
+    "S_EMAIL_LABEL": "Email",
+    "S_QUESTION_LABEL": "Question",
+    "S_START_BUTTON": "Démarrer le chat",
+    "S_SUBJECT_LABEL": "Sujette",
+    "S_CONTENT_LABEL": "Contenu",
+    "S_SEND_EMAIL_BUTTON": "Envoyer un e-mail"
+  },
+  "S_CONNECTING_PAGE": {
+    "S_CONTENT": "Veuillez patienter pendant que nous vous connectons à notre agent d'assistance en ligne."
+  },
+  "S_CONVERSATION_PAGE": {
+    "S_SEND_BUTTON": "Envoyer",
+    "S_MAIN_MENU_TITLE": "Menu principal",
+    "S_EXIT_CONFERENCE": "{0} a quitté la conversation",
+    "S_TRANSFER_GROUP": "Vous avez été transféré dans le groupe {0}.",
+    "S_AGENT_ADDED":"{0} added to conversation",
+    "S_AGENT_END":"L'agent a mis fin au chat.",
+    "S_CUSTOMER_END":"Customer ended the chat.",
+    "S_START_NEW": "Démarrer le chat",
+    "S_THANK_YOU_OR":"or",
+    "S_CLOSE_WINDOW":"Fermer la fenêtre",
+    "S_TYPING": "Typing...",
+    "S_VIDEO_SESSION_REQUEST_RECEIVED": "Video and/or Audio session request received",
+    "S_VIDEO_SESSION_CANCELED": "Video and/or Audio session canceled",
+    "S_VIDEO_SESSION_REJECTED": "Video and/or Audio session rejected",
+    "S_VIDEO_SESSION_ACCEPTED": "Video and/or Audio session accepted",
+    "S_VIDEO_ACTIVATED": "Video and/or Audio activated",
+    "S_VIDEO_TERMINATED": "Video and/or Audio terminated",
+    "S_INPUT_MESSAGE_PLACEHOLDER": "Type here...",
+    "S_CUSTOMER_REQUEST_TO_CHAT": "Hello.",
+    "S_TURN_SOUND_ON": "Turn Sound On",
+    "S_TURN_SOUND_OFF": "Turn Sound Off",
+    "S_PRINT_TRANSCRIPT": "Print Transcript",
+    "S_REQUEST_TRANSCRIPT": "Email Transcript...",
+    "S_TALK_TO_LIVE_AGENT": "Talk to Live Agent"
+  },
+  "S_ERROR_PAGE": {
+    "S_START_OVER": "Recommencer à zéro",
+    "S_SEND_EMAIL": "Envoyer un e-mail"
+  },
+  "S_TERMINATE_POPUP": {
+    "S_TERMINATE_BUTTON": "Terminer la conversation",
+    "S_TERMINATE_CONFIRM": "Êtes-vous sûr de vouloir mettre fin au chat?"
+  },
+  "S_ERRORS": {
+    "S_CONNECTING": "Nous rencontrons des problèmes de connexion. Veuillez réessayer plus tard.",
+    "S_WEB_SOCKET": "A required modern browser feature is not enabled or supported.<br /><br />Please ensure web socket feature is enabled in your browser.",
+    "S_LOCAL_STORAGE": "A required modern browser feature is not enabled or supported.<br /><br />Please ensure cookies and local storage are enabled in your browser.",
+    "S_LOCAL_STORAGE_SAFARI": "A required modern browser feature is not enabled or supported (or you are using a Private Window in Safari).<br /><br />Please ensure cookies and local storage are enabled.",
+    "S_IE_NOT_SUPPORTED": "La version actuelle de ce navigateur n'est pas prise en charge. Veuillez mettre à niveau vers Internet Explorer 11 ou supérieur.",
+    "S_BEFORE_UNLOAD": "Si vous rechargez cette page, vous quitterez la session de chat en cours.",
+    "S_NO_SERVICE": "Aucun agent n'est actuellement disponible. Réessayez plus tard ou envoyez un e-mail.",
+    "S_DUPLICATE_WEB_SOCKET": "Vous avez actuellement plus d'un chat ouvert. Veuillez utiliser la conversation d'origine.",
+    "S_NETWORK_LOST": "Connexion réseau perdue. Veuillez vérifier votre connexion réseau ou réessayer plus tard."
+  },
+  "S_MESSAGES": {
+    "S_TO": "L'agent {0} discute maintenant avec vous !",
+    "S_FROM": "L'agent {0} a transféré le chat à {1}",
+    "S_GR_IN_PROGRESS": "Nous rencontrons des problèmes de réseau. Il peut s'écouler jusqu'à 10 minutes avant que le problème de réseau soit résolu et que notre agent puisse répondre à vos messages. Merci pour votre patience."
+  },
+  "S_END_CHAT": "Terminer la conversation",
+  "S_CANCEL": "Annuler",
+  "S_SAVE": "Sauvegarder",
+  "S_VALIDATE": {
+    "S_REQUIRED": "Examen requis",
+    "S_INVALID_EMAIL": "Adresse e-mail invalide"
+  },
+   "S_VIDEO_POPUP": {
+    "S_ACCEPT": "Accepter",
+    "S_REJECT": "Rejeter",
+    "S_MESSAGE": "Video And/Or Audio Session Requested"
+  },
+  "S_CALLBACK_REQUEST": {
+    "S_REQUEST": "Demande de rappel",
+    "S_NUMBER": "Numéro de téléphone",
+    "S_REQUESTED": "Rappel demandé",
+    "S_CONFIRMATION": "Nous vous appellerons dès que possible au"
+  },
+  "S_REQUEST_TRANSCRIPT": {
+    "S_ERROR": "Une erreur s'est produite, veuillez réessayer.",
+    "S_SENT": "La transcription du chat sera envoyée à <b></b> une fois l'interaction terminée.",
+    "S_REQUEST": "Entrez une adresse email:",
+    "S_ENTER_EMAIL": "Adresse e-mail..."
+  },
+  "S_SENT_PAGE": {
+    "S_CONTENT": "Merci pour ton e-mail. nous vous contacterons dès que possible.",
+    "S_START_NEW": "Commencer un nouvel e-mail"
+  }
+});
