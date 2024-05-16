20116678e9e0d15c6ab663804266abc8ab336a18
dventura-tsr <98617967+dventura-tsr@users.noreply.github.com>
2024-05-16 15:08:39 -0700

    Add files via upload

---
+++ FrenchLanguage3.js
@@ -0,0 +1,91 @@
+chatLangCallback({
+    "APPLICATION_TYPE": "Clavardage",
+    "S_PAGE_TITLE": "Clavardage",
+    "S_PROVIDER": "<span class='provider-text'>Four par <span class='provider-five'>Five</span><span class='provider-nine'>9</span></span>",
+    "S_NO_NAME": "moi",
+    "S_INITIALIZATION_PAGE": {
+        "S_CONTENT": "Initialisation..."
+    },
+    "S_INFORMATION_PAGE": {
+        "S_PROFILE_LABEL": "Profil",
+        "S_NAME_LABEL": "Nom",
+        "S_EMAIL_LABEL": "Adresse courriel",
+        "S_QUESTION_LABEL": "Question",
+        "S_START_BUTTON": "Commencez à discuter",
+        "S_SEND_BUTTON": "Envoyer le sondage",
+        "S_COMMENTS_LABEL": "Commentaires"
+    },
+    "S_CONNECTING_PAGE": {
+        "S_CONTENT": "Vous êtes mis en relation avec un agent."
+    },
+    "S_CONVERSATION_PAGE": {
+        "S_SEND_BUTTON": "Envoyer",
+        "S_EXIT_CONFERENCE": "l'agent {0} a quitté la conférence.",
+        "S_TRANSFER_GROUP": "Vous avez été transféré au groupe {0}.",
+        "S_AGENT_END": "l'agent a mis fin au clavardage.",
+        "S_CUSTOMER_END": "le client a mis fin au clavardage.",
+        "S_THANK_YOU_END": "Merci.",
+        "S_START_NEW": "Démarrer une nouvelle clavardage",
+        "S_TYPING": "Taper...",
+        "S_VIDEO_SESSION_REQUEST_RECEIVED": "Demande de séance vidéo et/ou audio reçue.",
+        "S_VIDEO_SESSION_CANCELED": "Séance vidéo et/ou audio annulée.",
+        "S_VIDEO_SESSION_REJECTED": "Séance vidéo et/ou audio rejetée.",
+        "S_VIDEO_SESSION_ACCEPTED": "Séance vidéo et/ou audio acceptée.",
+        "S_VIDEO_ACTIVATED": "Séance vidéo et/ou audio activée.",
+        "S_VIDEO_TERMINATED": "Séance vidéo et/ou audio terminée.",
+        "S_INPUT_MESSAGE_PLACEHOLDER": "écrivez ici..."
+    },
+    "S_ERROR_PAGE": {
+        "S_START_OVER": "Recommencer",
+        "S_SEND_EMAIL": "Envoyer un courriel"
+    },
+    "S_TERMINATE_POPUP": {
+        "S_TERMINATE_BUTTON": "Fin",
+        "S_TERMINATE_CONFIRM": "Voulez-vous vraiment mettre fin au clavardage?"
+    },
+    "S_ERRORS": {
+        "S_CONNECTING": "Nous rencontrons des problèmes de connexion. Réessayez plus tard.",
+        "S_WEB_SOCKET": "La version actuelle de votre navigateur n'est pas prise en charge.<br /><br />Passez à un navigateur moderne.",
+        "S_LOCAL_STORAGE": "The current version of your browser is not supported.<br /><br />Passez à un navigateur moderne.",
+        "S_LOCAL_STORAGE_SAFARI": "La version actuelle de votre navigateur n'est pas prise en charge (ou vous utilisez une fenêtre privée dans Safari).<br /><br />Passez à un navigateur moderne.",
+        "S_BEFORE_UNLOAD": "Si vous rechargez cette page, vous quitterez la session de chat en cours.",
+        "S_NO_SERVICE": "Aucun agent n'est actuellement disponible. Réessayez plus tard ou envoyez un courriel.",
+        "S_NETWORK_LOST": "La connexion réseau a été perdue. Vérifiez votre connexion réseau ou réessayez plus tard."
+    },
+
+    "S_MESSAGES": {
+        "S_TO": "Agent {0} est maintenant en train de discuter avec vous.",
+        "S_FROM": "Agent {0} transféré le clavardage à {1}",
+        "S_GR_IN_PROGRESS": "Nous rencontrons des problèmes de réseau. Le problème peut durer jusqu'à 10 minutes. Notre agent répondra à votre message dans les plus brefs délais. Merci pour votre patience."
+
+
+    },
+    "S_END_CHAT": "Clavardage terminée",
+    "S_CANCEL": "Annuler",
+    "S_VALIDATE": {
+        "S_REQUIRED": "Required field",
+        "S_INVALID_EMAIL": "Invalid courriel address"
+    },
+    "S_VIDEO_POPUP": {
+        "S_ACCEPT": "Accepter",
+        "S_REJECT": "Rejeter",
+        "S_MESSAGE": "Séance vidéo et/ou audio demandée"
+    },
+    "S_THANK_YOU_OR": "Merci",
+    "S_CLOSE_WINDOW": "Fermer la fenêtre",
+    "S_CLOSE": "Fermer",
+    "S_CALLBACK_REQUEST": {
+        "S_REQUEST": "Demande",
+        "S_NUMBER": "Nombre",
+        "S_REQUESTED": "Demandée",
+        "S_CONFIRMATION": "Confirmation"
+    },
+    "S_WAITING_PAGE": {
+        "S_CONTENT": "Contenu"
+    },
+    "S_SENT_PAGE": {
+        "S_START_NEW_CHAT": "Démarrer une nouvelle discussion",
+        "S_START_NEW_EMAIL": "Démarrer un nouvel e-mail"
+    },
+    "S_PROVIDER": ""
+});
